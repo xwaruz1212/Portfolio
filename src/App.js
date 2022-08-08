40310e2d89fe217e18b64c999de4d3120d4c3c66
Kacper <kaczur190@gmail.com>
2022-08-08 12:51:50 +0200

    Building routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+import { Routes, Route } from 'react-router-dom'
+import Layout from './components/Layout'
+import Home from './components/Home'
+import AboutMe from './components/AboutMe'
+import Projects from './components/Projects'
+import ContactMe from './components/ContactMe'
 
 
 function App() {
   return (
-    <div>
-      Portfolio
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="AboutMe" element={<AboutMe />} />
+          <Route path="Projects" element={<Projects />} /> 
+          <Route path="ContactMe" element={<ContactMe />} /> 
+        </Route>
+      </Routes>
+    </>
   );
 }
 
---
+++ src/components/Layout/index.js
@@ -0,0 +1,13 @@
+import Navbar from "../Navbar";
+import './index.scss'
+
+
+const Layout = () => {
+    return ( 
+        <>
+        <Navbar/>
+        </>
+     );
+}
+ 
+export default Layout;
