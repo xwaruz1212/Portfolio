243ae55943dec9be4fa74c7996409e1e4f8e69b3
Kacper <kaczur190@gmail.com>
2022-08-15 22:44:06 +0200

    Layout and Navbar design changed

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,17 +1,16 @@
-import Navbar from "../Navbar";
-import { Outlet } from "react-router-dom";
+import { Outlet } from 'react-router-dom'
+import Navbar from '../Navbar/'
 import './index.scss'
 
-
 const Layout = () => {
-    return (
-        <div className="App">
-            <Navbar/>
-            <div className="page">
-                <Outlet/>
-            </div>
-        </div>
-     )
+  return (
+    <div className="App">
+      <Navbar />
+      <div className="Page">
+        <Outlet />
+      </div>
+    </div>
+  );
 }
- 
+
 export default Layout;
