6adfff009d2ad79451e4adcca338ac7c2ef06b52
Kacper <kaczur190@gmail.com>
2022-08-16 16:03:32 +0200

    experience page added with icons and styles

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,5 +1,35 @@
+import {
+    faCss3,
+    faHtml5,
+    faJsSquare,
+    faReact,
+    faSass
+  } from '@fortawesome/free-brands-svg-icons'
+  import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+  import PixiJs from '../../assets/images/Pixi.png'
+  import './index.scss'
+
 const Experience = () => {
-    return ( <div>hello</div> );
+    return (
+        <>
+            <div className='page-cont'>
+                <h1 className='text'>
+                    Experience
+                </h1>
+                <div className='icons'>
+                    <h1 className='one'><FontAwesomeIcon icon={faReact}/></h1>
+                    <h1 className='two'><FontAwesomeIcon icon={faJsSquare}/></h1>
+                    <h1 className='three'><FontAwesomeIcon icon={faHtml5}/></h1>
+                    <br />
+                    <h1 className='four'><FontAwesomeIcon icon={faCss3}/></h1>
+                    <h1 className='five'><FontAwesomeIcon icon={faSass}/></h1>
+                    <h1 className='six'><img src={PixiJs} alt="" /></h1>
+                </div>
+            
+            </div> 
+        </>
+        
+    );
 }
  
 export default Experience;
