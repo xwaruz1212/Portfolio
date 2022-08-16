d2b84d7b3b0b84101357683604a087d3ed5a5d5a
Kacper <kaczur190@gmail.com>
2022-08-16 15:08:14 +0200

    About Me page designed and added

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,5 +1,49 @@
+import { useState } from 'react'
+import Me from '../../assets/images/Me.jpeg'
+import Me2 from '../../assets/images/Me2.jpeg'
+import './index.scss'
+
 const About = () => {
-    return ( <div>hello</div> );
+const [nextImage, setNextImage] = useState(true);
+
+
+    return ( 
+        <>
+            <div className='page-container'>
+                <div className="title">
+                    <h1>
+                        About Me
+                    </h1>
+                </div> 
+                <p className='about-text'>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        Error deserunt, minus voluptatum voluptatibus illum, velit, 
+                        obcaecati eaque ipsam tempore harum commodi ducimus. 
+                        Porro impedit excepturi veniam officiis, voluptate sint mollitia.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        Error deserunt, minus voluptatum voluptatibus illum, velit, 
+                        obcaecati eaque ipsam tempore harum commodi ducimus. 
+                        Porro impedit excepturi veniam officiis, voluptate sint mollitia.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        Error deserunt, minus voluptatum voluptatibus illum, velit, 
+                        obcaecati eaque ipsam tempore harum commodi ducimus. 
+                        Porro impedit excepturi veniam officiis, voluptate sint mollitia.
+                        
+                    </p>
+                <div className='img-cont'>
+                     <button onClick={() => setNextImage(!nextImage)}>  
+                        {nextImage ? (
+                            <img src={Me} alt="" />
+                        ) : (
+                            <img src={Me2} alt=""/>
+                        )
+                        }
+                    </button>
+                </div>
+            </div>
+        </>
+        
+        );
 }
  
 export default About;
