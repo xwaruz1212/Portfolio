138c3ea013c8cb07a8856ff8d88f8c11f91801d7
Kacper <kaczur190@gmail.com>
2022-08-16 23:42:57 +0200

    experience page added with stylesheet

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -17,13 +17,52 @@ const Experience = () => {
                     Experience
                 </h1>
                 <div className='icons'>
-                    <h1 className='one'><FontAwesomeIcon icon={faReact}/></h1>
-                    <h1 className='two'><FontAwesomeIcon icon={faJsSquare}/></h1>
-                    <h1 className='three'><FontAwesomeIcon icon={faHtml5}/></h1>
+                    <div className='one'><FontAwesomeIcon icon={faReact}/>
+                    <div className='overlay'>
+                        <p className='p-text'>
+                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+                            Eligendi eaque facere numquam natus nisi. 
+                            Commodi doloremque explicabo laborum, fugiat enim, soluta possimus, 
+                            natus dolore blanditiis magni vel. Nemo, hic harum.
+                        </p>
+                    </div>
+                    </div>
+                    <div className='two'><FontAwesomeIcon icon={faJsSquare}/>
+                    <div className='overlay'>
+                        <p className='p-text'>
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo alias possimus quasi aut iusto quod quas assumenda atque amet, sapiente nobis aspernatur animi. Porro, voluptates. Obcaecati, quae. Reiciendis, pariatur quibusdam?
+                        </p>
+                    </div>
+                    </div>
+                    <div className='three'><FontAwesomeIcon icon={faHtml5}/>
+                    <div className='overlay'>
+                        <p className='p-text'>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, facere ipsum fugiat vero facilis placeat enim, consequatur commodi mollitia ut nulla hic dignissimos earum ipsa repellat qui nostrum sed quisquam?
+                        </p>
+                    </div>
+                    </div>
                     <br />
-                    <h1 className='four'><FontAwesomeIcon icon={faCss3}/></h1>
-                    <h1 className='five'><FontAwesomeIcon icon={faSass}/></h1>
-                    <h1 className='six'><img src={PixiJs} alt="" /></h1>
+                    <div className='four'><FontAwesomeIcon icon={faCss3}/>
+                    <div className='overlay'>
+                        <p className='p-text'>
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas nobis a dolores ea asperiores. Tempore voluptates animi facere nesciunt quisquam fugit omnis ipsam placeat, rem nihil sunt necessitatibus impedit. Itaque.
+                        </p>
+                    </div>
+                    </div>
+                    <div className='five'><FontAwesomeIcon icon={faSass}/>
+                    <div className='overlay'>
+                        <p className='p-text'>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto facilis odio magni quibusdam, quia, illum, quidem possimus aspernatur tempore in illo sed molestiae rerum maxime dolore. Ea nemo sequi dignissimos.
+                        </p>
+                    </div>
+                    </div>
+                    <div className='six'><img src={PixiJs} alt="" />
+                    <div className='overlay'>
+                        <p className='p-text'>
+                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa itaque, beatae ex esse ipsum doloremque cupiditate magni obcaecati explicabo, molestias deleniti maxime dolore eligendi cum quam, est ab quaerat quidem?
+                        </p>
+                    </div>
+                    </div>
                 </div>
             
             </div> 
