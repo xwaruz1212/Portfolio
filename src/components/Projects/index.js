1492a787c1314f30a1a3368a93536f3472bfaf95
Kacper <kaczur190@gmail.com>
2022-08-17 11:56:06 +0200

    text added

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -20,24 +20,28 @@ const Experience = () => {
                     <div className='one'><FontAwesomeIcon icon={faReact}/>
                     <div className='overlay'>
                         <p className='p-text'>
-                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
-                            Eligendi eaque facere numquam natus nisi. 
-                            Commodi doloremque explicabo laborum, fugiat enim, soluta possimus, 
-                            natus dolore blanditiis magni vel. Nemo, hic harum.
+                            I started learning React framework at the beginning of 2022. So far I have finished React course on Udemy and created a few small projects. 
+                            Additionally I created and designed from scratch a website gallery commissioned project with pdf books, which you can check 
+                            <a href="https://makislazos.netlify.app/" target="_blank" rel="noopener noreferrer"> here</a>
                         </p>
                     </div>
                     </div>
                     <div className='two'><FontAwesomeIcon icon={faJsSquare}/>
                     <div className='overlay'>
                         <p className='p-text'>
-                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo alias possimus quasi aut iusto quod quas assumenda atque amet, sapiente nobis aspernatur animi. Porro, voluptates. Obcaecati, quae. Reiciendis, pariatur quibusdam?
+                            My journey with JavaScript began in the November 2021. 
+                            At the beginning I was learning JS for hobby, but after some time I've realized that is what I want to do in my life. 
+                            I've created some projects in pure JavaScript which You can check at my Github profile 
+                            <a href="https://github.com/xwaruz1212" target="_blank" rel="noreferrer"> here</a>
                         </p>
                     </div>
                     </div>
                     <div className='three'><FontAwesomeIcon icon={faHtml5}/>
                     <div className='overlay'>
                         <p className='p-text'>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, facere ipsum fugiat vero facilis placeat enim, consequatur commodi mollitia ut nulla hic dignissimos earum ipsa repellat qui nostrum sed quisquam?
+                            Html is my first experience with coding. 
+                            I started getting to know html in secondary school and time to time I've done few basics projects. 
+                            When I've started with JavaScript and React I've also started over with Html and extended my basic knowledge. 
                         </p>
                     </div>
                     </div>
@@ -45,21 +49,21 @@ const Experience = () => {
                     <div className='four'><FontAwesomeIcon icon={faCss3}/>
                     <div className='overlay'>
                         <p className='p-text'>
-                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas nobis a dolores ea asperiores. Tempore voluptates animi facere nesciunt quisquam fugit omnis ipsam placeat, rem nihil sunt necessitatibus impedit. Itaque.
+                            CSS is stylesheet which I've discovered when I was doing some projects in html and in the end of 2021 I've started over with this technology.
                         </p>
                     </div>
                     </div>
                     <div className='five'><FontAwesomeIcon icon={faSass}/>
                     <div className='overlay'>
                         <p className='p-text'>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto facilis odio magni quibusdam, quia, illum, quidem possimus aspernatur tempore in illo sed molestiae rerum maxime dolore. Ea nemo sequi dignissimos.
+                            I found out about sass when I was expanding my knowledge of CSS and since then I am learning and doing some projects to be advanced in that technology. 
                         </p>
                     </div>
                     </div>
                     <div className='six'><img src={PixiJs} alt="" />
                     <div className='overlay'>
                         <p className='p-text'>
-                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa itaque, beatae ex esse ipsum doloremque cupiditate magni obcaecati explicabo, molestias deleniti maxime dolore eligendi cum quam, est ab quaerat quidem?
+                            PixiJs is the first library of JS that I've learned. I've created a basic slot machine game by the beginning of 2022.
                         </p>
                     </div>
                     </div>
