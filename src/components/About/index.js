c306e43a631503898c748748604942ce2f148a51
Kacper <kaczur190@gmail.com>
2022-08-18 02:36:17 +0200

    added mobile version for homepage and about

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import Me from '../../assets/images/Me.jpeg'
-import Me2 from '../../assets/images/Me2.jpeg'
+import Me2 from '../../assets/images/Me.jpeg'
+import Me from '../../assets/images/Me2.png'
 import './index.scss'
 
 const About = () => {
@@ -16,18 +16,13 @@ const [nextImage, setNextImage] = useState(true);
                     </h1>
                 </div> 
                 <p className='about-text'>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                        Error deserunt, minus voluptatum voluptatibus illum, velit, 
-                        obcaecati eaque ipsam tempore harum commodi ducimus. 
-                        Porro impedit excepturi veniam officiis, voluptate sint mollitia.
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                        Error deserunt, minus voluptatum voluptatibus illum, velit, 
-                        obcaecati eaque ipsam tempore harum commodi ducimus. 
-                        Porro impedit excepturi veniam officiis, voluptate sint mollitia.
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                        Error deserunt, minus voluptatum voluptatibus illum, velit, 
-                        obcaecati eaque ipsam tempore harum commodi ducimus. 
-                        Porro impedit excepturi veniam officiis, voluptate sint mollitia.
+                        <h2 className='h3'>Hi! My name is Kacper. I am passionate programmer from Poland. <br></br></h2>
+                        I started learning programming in 2020. At first it was only a hobby, but this year it became my passion.
+                        I had some short episodes with C and C++ and then finally I discovered web development. 
+                        I decided that this is the thing that I want to do and even resigned from  law studies, to fully focus on programming. 
+                        I learnt almost everything on my own, I had to be disciplined, organized and creative. I also have got wide field of interest outside programming
+                        - I got some experience with law, social studies and marketing. In my free time I travel, play football and tenis.
+                        <br></br>I am still at the begining, but I learn fast and I believe I could be a real asset in your team. I would be happy to prove that on the interview!
                         
                     </p>
                 <div className='img-cont'>
