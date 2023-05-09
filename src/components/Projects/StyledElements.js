b94d3c6b15c31c653fa31adee8878692e3adc097
Kacper <kaczur190@gmail.com>
2023-05-09 15:47:33 +0200

    Projects added

--- src/components/Projects/StyledElements.js
+++ src/components/Projects/StyledElements.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 export const LoadingPage = styled.div`
@@ -21,4 +22,85 @@ export const LoadingText = styled.h1`
     font-size: 5vh;
     color: white;
     cursor: pointer;
+`;
+
+export const ProjectsContainer = styled.div`
+    background: rgb(54, 119, 219);
+    background: linear-gradient(
+    90deg,
+    rgba(12, 22, 79, 1) 0%,
+    rgba(29, 49, 117, 1) 50%,
+    rgba(49, 59, 100, 1) 100%);
+    width: 100%;
+    height: 100%;
+    align-items: center;
+    text-align: center;
+    justify-items: center;
+    position: absolute;
+    display: flex;
+    flex-direction: column;
+`;
+
+export const ProjectsText = styled.h1`
+    position: relative;
+    font-size: 7vh;
+    top: 5vh;
+    color: white;
+    text-shadow: 2px 2px grey;
+`;
+
+export const ProjectsSection = styled.div`
+    display: flex;
+    flex-direction: row;
+    width: 80vw;
+    height: 80vh;
+    background-color: transparent;
+    position: relative;
+    margin-top: 5vh;
+    gap: 3vw;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-items: center;
+    justify-content: center;
+`;
+
+export const ProjectsCard = styled.a`
+    height: 30vh;
+    width: 30vw;
+    background-color: transparent;
+    color: white;
+    border-radius: 1rem;
+    overflow: hidden;
+    cursor: pointer;
+    align-items: center;
+    justify-items: center;
+    border: solid 2px white;
+    transition-duration: 0.2s;
+
+    :hover{
+        height: 31vh;
+        width: 31vw; 
+    }
+
+    & > img{
+        width: 100%;
+        max-height: 100%;
+    }
+
+    & > p {
+        font-size: 3vh;
+        text-align: center;
+        background-color: transparent;
+        margin-top: 5vh;
+        padding: 2px;
+    }
+`;
+
+export const BackLink = styled(Link)`
+    font-size: 4vh;
+    position: absolute;
+    margin: 2px 2px;
+    text-decoration: none;
+    left: 0;
+    color: white;
 `;
--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -2,12 +2,27 @@ import React from 'react'
 import { useEffect } from 'react';
 import { useState } from 'react';
 import { Blocks } from 'react-loader-spinner';
-import { LoadingPage, LoadingText } from './StyledElements';
-
+import { BackLink, LoadingPage, LoadingText, ProjectsCard, ProjectsContainer, ProjectsSection, ProjectsText } from './StyledElements';
+import MWebsite from '../../images/mwebsite.png'
+import SMachine from '../../images/slot_machine.gif'
+import OS from '../../images/onestop.png'
+import PB from '../../images/progress.jpg'
 
 const Projects = () => {
 
     const [loading, setLoading] = useState(false);
+    const [active, setActive] = useState(false);
+
+    const ShowText = () => {
+        setActive(!active)
+    };
+
+    const [active2, setActive2] = useState(false);
+
+    const ShowText2 = () => {
+        setActive2(!active2)
+    };
+
 
     useEffect(() => {
         setLoading(true);
@@ -25,7 +40,35 @@ const Projects = () => {
             ) :
                 (
                     <>
-                        <div>Projects</div>
+                        <ProjectsContainer>
+                            <BackLink to="/">Back to main page</BackLink>
+                            <ProjectsText>Projects</ProjectsText>
+                            <ProjectsSection>
+                                <ProjectsCard href='https://makislazos.netlify.app/'
+                                    target="_blank"
+                                    rel="noreferrer" >
+                                    <img src={MWebsite} alt="" />
+                                </ProjectsCard>
+                                <ProjectsCard href='https://github.com/xwaruz1212/Slot-Machine'
+                                    target="_blank"
+                                    rel="noreferrer">
+                                    <img src={SMachine} alt="" />
+                                </ProjectsCard>
+                                <ProjectsCard onClick={ShowText}>
+                                    {active ? (
+                                        <p>Since last year I am working part time for a company from London. I have oportunity to work with different technologies such as Javascript, React, MYSQL, PHP, Docker.</p>
+                                    )
+                                        :
+                                        <img src={OS} alt="" />
+                                    } </ProjectsCard>
+                                <ProjectsCard onClick={ShowText2}>{active2 ? (
+                                    <p>I am working on website for building company based in UK. I am developing it with React and Node.</p>
+                                )
+                                    :
+                                    <img src={PB} alt="" />
+                                }</ProjectsCard>
+                            </ProjectsSection>
+                        </ProjectsContainer>
                     </>
                 )}
         </div>
