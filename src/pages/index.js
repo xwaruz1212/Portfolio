bcd5e5d828ab54911bb3dd28e50556973aefca86
Kacper <kaczur190@gmail.com>
2023-05-09 15:47:33 +0200

    Projects added

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,22 +5,44 @@ import Skills from "../components/Experience";
 import Navbar from "../components/Navbar";
 import Sidebar from "../components/Sidebar";
 import StartSection from "../components/StartSection";
+import { LoadingPage, LoadingText } from "../components/Projects/StyledElements";
+import { Blocks } from "react-loader-spinner";
+import { useEffect } from "react";
 
 const Home = () => {
+
+  const [loading, setLoading] = useState(false);
   const [isOpen, setisOpen] = useState(false);
   const toggle = () => {
     setisOpen(!isOpen);
   };
 
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
+
   return (
-    <>
-      <Navbar toggle={toggle} />
-      <Sidebar isOpen={isOpen} toggle={toggle} />
-      <StartSection />
-      <About />
-      <Skills />
-      <Contact />
-    </>
+    <div>
+      {loading ? (
+        <LoadingPage>
+          <Blocks size={200} color={'white'} loading={loading} />
+          <LoadingText>Page is loading...</LoadingText>
+        </LoadingPage>
+      ) : (
+        <>
+          <Navbar toggle={toggle} />
+          <Sidebar isOpen={isOpen} toggle={toggle} />
+          <StartSection />
+          <About />
+          <Skills />
+          <Contact />
+        </>
+      )
+      }
+    </div>
   );
 };
 
